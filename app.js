b455d8335aba68ad4cad882d9c339d22da7865ee
Ifedayo Ijabadeniyi <ifedayoadeniyi@gmail.com>
2021-06-21 23:26:50 +0100

    Connect mongo database  🍕

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({path: './config/config.env'})
 
+connectDB()
+
 const app = express();
+
+// Logging
+process.env.NODE_ENV === "development" && app.use(morgan('dev'))
 
 const PORT = process.env.PORT || 3000;
 
